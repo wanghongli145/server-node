58ba32ec1e1fe53f730695ee90fda8761dbffdb0
wanghl <15761674230@163.com>
2018-10-17 23:28:44 +0800

    async

--- server.js
+++ server.js
@@ -1,30 +1,46 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const logger = require('morgan');
-const errorhandler = require('errorhandler');
+// p40
 const mongodb = require('mongodb');
+const customers = require('./customer-data.json');
+const customerAddresses= require('./customer-address-db.json');
+const async = require('async');
+const url = 'mongodb://localhost:27017/customer-db';
 
-const url = 'mongodb://localhost:27017/my-first-db';
-const app = express();
+let tasks = [];
+const limit = parseInt(process.argv[2], 10) || 1000;
+mongodb.MongoClient.connect(url, {
+      useNewUrlParser: true
+    }, function (error, db) {
+  if (error) {
+    return process.exit(1);
+  }
+  const dataBase = db.db('customer-address-db');
+  customers.forEach((customer, index, list) => {
+    customers[index] = Object.assign(customer, customerAddresses[index]);
+    if (index % limit === 0) {
+      const start = index;
+      const end = (start + limit > customers.length) ? customers.length - 1 : start +limit;
+      tasks.push((done) => {
+        dataBase.collection('customers').insert(customers.slice(start, end), (error, results) => {
+          done(error, results);
+        })
+      })
+    }
+  })
+  
+  console.log(`Launching ${tasks.length} paraller task(s)`);
+  const startTime = Date.now();
+  async.parallel(tasks, (error, results) => {
+    if (error) {
+      console.error(error);
+      const endTime = Date.now();
+      console.log(`Execution time: ${endTime - startTime}`);
+    }
+  })
+  
 
-app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: false}));
-app.use(logger('dev'));
-app.use(errorhandler());
-app.use(express.static(path.join(__dirname, '/')));
 
-mongodb.MongoClient.connect(url, function (err, db) {
-  console.log(db);
-  const dataBase = db.db('my-first-db');
-
-  app.use('/', (req, res, next) => {
-    // console.log(req);
-    next();
-  });
-
-  const port = 7001;
-  app.use(errorhandler())
-  app.listen(port);
-  console.log(`Your server is running in localhost:${port}`)
+  // const port = 7001;
+  // app.use(errorhandler())
+  // app.listen(port);
+  // console.log(`Your server is running in localhost:${port}`)
 })
